437830414c49cf6f54e87d9ea86e2d17e3aaddca
dilshad1997 <ahmad.dilshad1997@gmail.com>
2025-11-08 10:37:33 +0530

    Initial Flutter WebView Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(SimpleBrowser());
-
-class SimpleBrowser extends StatefulWidget {
-  @override
-  State<SimpleBrowser> createState() => _SimpleBrowserState();
+void main() {
+  runApp(const SimpleBrowser());
 }
 
-class _SimpleBrowserState extends State<SimpleBrowser> {
-  late WebViewController _controller;
-  final TextEditingController _urlController =
-      TextEditingController(text: 'https://google.com');
+class SimpleBrowser extends StatelessWidget {
+  const SimpleBrowser({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Simple Browser',
-      home: Scaffold(
-        appBar: AppBar(
-          title: TextField(
-            controller: _urlController,
-            decoration: InputDecoration(
-              hintText: 'Enter URL (e.g. https://openai.com)',
-              suffixIcon: IconButton(
-                icon: Icon(Icons.search),
-                onPressed: _loadUrl,
-              ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+  bool _isLoading = true;
+  final String homeUrl = "https://google.com";
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(homeUrl))
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageFinished: (_) => setState(() => _isLoading = false),
+      ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("🌐 Simple Browser"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => _controller.reload(),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_isLoading)
+            const Center(child: CircularProgressIndicator()),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blueAccent,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () async {
+                if (await _controller.canGoBack()) _controller.goBack();
+              },
             ),
-            onSubmitted: (_) => _loadUrl(),
-          ),
-        ),
-        body: SafeArea(
-          child: WebViewWidget(
-            controller: _controller = WebViewController()
-              ..setJavaScriptMode(JavaScriptMode.unrestricted)
-              ..loadRequest(Uri.parse(_urlController.text)),
-          ),
+            IconButton(
+              icon: const Icon(Icons.home, color: Colors.white),
+              onPressed: () => _controller.loadRequest(Uri.parse(homeUrl)),
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward, color: Colors.white),
+              onPressed: () async {
+                if (await _controller.canGoForward()) _controller.goForward();
+              },
+            ),
+          ],
         ),
       ),
     );
-  }
-
-  void _loadUrl() {
-    String url = _urlController.text.trim();
-    if (!url.startsWith('http')) url = 'https://$url';
-    _controller.loadRequest(Uri.parse(url));
   }
 }
